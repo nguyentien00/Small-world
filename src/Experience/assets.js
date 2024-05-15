2235af1527b857205f749b211336a4d47ff0bdde
nguyentien00 <149949507+nguyentien00@users.noreply.github.com>
2024-05-15 13:20:01 +0700

    Add files via upload

---
+++ src/Experience/assets.js
@@ -0,0 +1,35 @@
+export default [
+    {
+        name: 'base',
+        data: {},
+        items:
+        [
+            { name: 'model', source: '/assets/model.glb' },
+            
+            { name: 'entranceModel', source: '/assets/entranceModel.glb' },
+
+            { name: 'bakedFloorLightModel', source: '/assets/bakedFloorLightModel.glb' },
+            { name: 'bakedFloorLightTexture', source: '/assets/bakedFloorLightTexture.png', type: 'texture' },
+            
+            { name: 'bakedFloorShadowModel', source: '/assets/bakedFloorShadowModel.glb' },
+            { name: 'bakedFloorShadowTexture', source: '/assets/bakedFloorShadowTexture.png', type: 'texture' },
+            
+            { name: 'entranceStrokeTexture', source: '/assets/entranceStrokeTexture.png', type: 'texture' },
+
+            { name: 'windStroke1Model', source: '/assets/windStroke1Model.glb' },
+            { name: 'windStroke2Model', source: '/assets/windStroke2Model.glb' },
+
+            { name: 'brassMatcapTexture', source: '/assets/matcaps/brass.png', type: 'texture' },
+            { name: 'fabricPinkDarkMatcapTexture', source: '/assets/matcaps/fabricPinkDark.png', type: 'texture' },
+            { name: 'fabricPinkLightMatcapTexture', source: '/assets/matcaps/fabricPinkLight.png', type: 'texture' },
+            { name: 'foliageMatcapTexture', source: '/assets/matcaps/foliage.png', type: 'texture' },
+            { name: 'globeMatcapTexture', source: '/assets/matcaps/globe.png', type: 'texture' },
+            { name: 'lensMatcapTexture', source: '/assets/matcaps/lens.png', type: 'texture' },
+            { name: 'metalMatcapTexture', source: '/assets/matcaps/metal.png', type: 'texture' },
+            { name: 'paperMatcapTexture', source: '/assets/matcaps/paper.png', type: 'texture' },
+            { name: 'stoneMatcapTexture', source: '/assets/matcaps/stone.png', type: 'texture' },
+            { name: 'stoneDarkMatcapTexture', source: '/assets/matcaps/stoneDark.png', type: 'texture' },
+            { name: 'woodMatcapTexture', source: '/assets/matcaps/wood.png', type: 'texture' },
+        ]
+    }
+]
